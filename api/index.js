4fdf031c6035b9a926beb318e929e48671e79304
Ravindra Nath Jha <ravindranathjha76@gmail.com>
2023-09-13 02:28:01 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('Connected to MongoDB');
+})
+.catch((err)=>{
+    console.error(`Error connecting to database: ${err}`);
+});
+
+// add password after username and add project name after .net/
 
 const app = express();
 
