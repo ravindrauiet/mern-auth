0baebc41e342ea83cbadb0f0c533afb3ea700395
Ravindra Nath Jha <ravindranathjha76@gmail.com>
2023-09-29 17:35:30 +0530

    update header component and make profile private and make dynamic navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
 
@@ -18,7 +19,9 @@ export default function App() {
     <Route path ="/about" element={<About />} />
     <Route path ="/sign-in" element={<SignIn />} />
     <Route path ="/sign-up" element={<SignUp />} />
-    <Route path ="/profile" element={<Profile />} />
+    <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+    </Route>
 
   </Routes>
 
